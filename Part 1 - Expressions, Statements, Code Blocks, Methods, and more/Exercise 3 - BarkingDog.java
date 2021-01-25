b118b4282a24fa20855bc397cb77d59efbde9bb2
josecgomezvazquez <40777679+josecgomezvazquez@users.noreply.github.com>
2021-01-25 15:14:54 -0600

    Create Exercise 3 - BarkingDog.java

---
+++ Part 1 - Expressions, Statements, Code Blocks, Methods, and more/Exercise 3 - BarkingDog.java
@@ -0,0 +1,18 @@
+public class BarkingDog 
+{
+    public static boolean shouldWakeUp(boolean isBarking, int hourOfDay)
+    {
+        if (hourOfDay < 0 || hourOfDay > 23)
+        {
+            return false;
+        }
+        if (isBarking && hourOfDay < 8 || hourOfDay > 22)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
