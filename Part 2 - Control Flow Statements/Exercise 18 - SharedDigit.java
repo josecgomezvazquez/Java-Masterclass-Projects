3cfff1a2c9e37f0a7c5bb345ce49700dec07f790
Jose Carlos Gomez-Vazquez <40777679+josecgomezvazquez@users.noreply.github.com>
2021-01-27 14:15:03 -0600

    Create Exercise 18 - SharedDigit.java

---
+++ Part 2 - Control Flow Statements/Exercise 18 - SharedDigit.java
@@ -0,0 +1,15 @@
+public class SharedDigit 
+{
+    public static boolean hasSharedDigit(int x, int y)
+    {
+        if ((x < 10 || x > 99) || (y < 10 || y > 99))
+        {
+            return false;
+        }
+        if (((x % 10) == (y % 10)) || ((x / 10) == (y / 10)) ||((x / 10) == (y % 10)) || ((x % 10) == (y / 10)))
+        {
+              return true;  
+        }
+        return false;
+    }
+}
